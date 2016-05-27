70eae63ddeb25970cde0ebb8bdf194d157c618a3
pjdufour <pjdufour.dev@gmail.com>
2016-05-27 12:52:03 +0200

    fixes to formatInteger

--- geosite/static/geosite/src/geosite/plugins/base/filters/formatInteger.js
+++ geosite/static/geosite/src/geosite/plugins/base/filters/formatInteger.js
@@ -2,17 +2,24 @@ geosite.filters["formatInteger"] = function()
 {
   return function(value, type, delimiter)
   {
-    if(type == "delimited")
+    if(value != undefined && value != "")
     {
-      delimiter = delimiter || ',';
-      var str = value.toString();
-      var pattern = new RegExp('(\\d+)(\\d{3})','gi');
-      while(pattern.test(str)){str=str.replace(pattern,'$1'+ delimiter +'$2');}
-      return str;
+      if(type == "delimited")
+      {
+        delimiter = delimiter || ',';
+        var str = Math.round(value).toString(); // Round in case value is a float
+        var pattern = new RegExp('(\\d+)(\\d{3})','gi');
+        while(pattern.test(str)){str=str.replace(pattern,'$1'+ delimiter +'$2');}
+        return str;
+      }
+      else
+      {
+        return Math.round(value).toString();
+      }
     }
     else
     {
-      return value.toString();
+        return "";
     }
   };
 };
